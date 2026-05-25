f491957ca572fe4e097d4541ab589c6fbb6eb9a0
WinnerSamurai6064 <ikegbuneagwu@gmail.com>
2026-05-25 15:08:40 +0100

    Route /admin to FrontDesk CMS

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import Admin from './Admin.jsx';
 import './styles.css';
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+const isAdminRoute = window.location.pathname.startsWith('/admin');
+
+root.render(isAdminRoute ? <Admin /> : <App />);
